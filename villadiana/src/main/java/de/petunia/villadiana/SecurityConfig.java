954a6a0484933079d9fb3eca2daca8902e4cd4c8
Felix Roske <felix@zalando.de>
2025-06-23 01:45:54 +0200

    Play with security config

--- villadiana/src/main/java/de/petunia/villadiana/SecurityConfig.java
+++ villadiana/src/main/java/de/petunia/villadiana/SecurityConfig.java
@@ -1,12 +1,14 @@
 package de.petunia.villadiana;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,8 +18,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebSecurity
 public class SecurityConfig {
 
-//    @Autowired
-//    private ClientRegistrationRepository clientRegistrationRepository;
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -25,10 +27,10 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                    .allowedOriginPatterns("http://localhost:3000", "http://alpicola")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                    .allowCredentials(true)
-                    .allowedHeaders("*");
+                        .allowedOriginPatterns("http://localhost:3000", "http://alpicola")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true)
+                        .allowedHeaders("*");
             }
         };
     }
@@ -51,20 +53,29 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/actuator/**").permitAll()
-                .requestMatchers("/api/**").authenticated()
-            )
-            .oauth2Login(Customizer.withDefaults()) // Nur wenn du Login UI brauchst
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/api/**").authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults()) // Nur wenn du Login UI brauchst
+//                .oauth2Login(oauth -> oauth
+//                         .loginPage("/oauth2/authorization/keycloak"))   // Einstiegspunkt fürs SPA
 //            .csrf(AbstractHttpConfigurer::disable) // Vorsicht bei echten POSTs!
 //            .sessionManagement(sm -> sm
 //                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
 //            )
-            .exceptionHandling(eh -> eh
-                .authenticationEntryPoint((req, res, authException) -> {
-                    res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthenticated");
-                })
-            );
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            request.getSession().invalidate();
+                            response.setStatus(HttpServletResponse.SC_OK);
+                        }).permitAll()
+                )
+                .exceptionHandling(eh -> eh
+                        .authenticationEntryPoint((req, res, authException) -> {
+                            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthenticated");
+                        })
+                );
 //            .cors(Customizer.withDefaults());
 
         return http.build();
