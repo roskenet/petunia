b891c068db9667eff3abd9b836c4314ff2abbc31
Felix Roske <felix@zalando.de>
2025-06-22 18:19:10 +0200

    Add antd

--- alpicola/src/app/page.tsx
+++ alpicola/src/app/page.tsx
@@ -1,10 +1,25 @@
-// This is the landing page
+"use client";
+
+import { Typography, Layout, Space } from 'antd';
+
+const { Title, Paragraph } = Typography;
+const { Content } = Layout;
 
 export default function Home() {
   return (
-  <>
-    <h1>Hello World!</h1>
-    <p>This is my webservice garden!</p>
-  </>
+    <Layout style={{ minHeight: '100vh', background: 'var(--background)' }}>
+      <Content style={{ 
+        padding: '50px', 
+        display: 'flex', 
+        flexDirection: 'column', 
+        justifyContent: 'center', 
+        alignItems: 'center' 
+      }}>
+        <Space direction="vertical" align="center">
+          <Title level={1}>Hello World!</Title>
+          <Paragraph>This is my webservice garden!</Paragraph>
+        </Space>
+      </Content>
+    </Layout>
   );
 }
