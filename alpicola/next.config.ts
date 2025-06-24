425b5e8b90eddfc4f06cc01ae25e0c844de32e3d
Felix Roske <felix@zalando.de>
2025-06-24 23:30:38 +0200

    Working minikube deployment

--- alpicola/next.config.ts
+++ alpicola/next.config.ts
@@ -1,32 +1,34 @@
 import type { NextConfig } from "next";
 
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       // API Requests
       {
         source: "/api/:path*",
-        destination: "http://localhost:8080/api/:path*",
+        destination: `${API_BASE_URL}/api/:path*`,
       },
       // OAuth2 Login Endpoint
       {
         source: "/oauth2/:path*",
-        destination: "http://localhost:8080/oauth2/:path*",
+        destination: `${API_BASE_URL}/oauth2/:path*`,
       },
       // Spring Security Login Redirect Handler
       {
         source: "/login/:path*",
-        destination: "http://localhost:8080/login/:path*",
+        destination: `${API_BASE_URL}/login/:path*`,
       },
       // Logout
       {
         source: "/logout",
-        destination: "http://localhost:8080/logout",
+        destination: `${API_BASE_URL}/logout`,
       },
       // Eventuell CSRF Token etc.
       {
         source: "/error",
-        destination: "http://localhost:8080/error",
+        destination: `${API_BASE_URL}/error`,
       },
     ];
   },
--- villadiana/src/main/java/de/petunia/villadiana/SecurityConfig.java
+++ villadiana/src/main/java/de/petunia/villadiana/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInit
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -59,7 +60,8 @@ public class SecurityConfig {
                 .oauth2Login(oauth -> oauth
                         .loginPage("/oauth2/authorization/keycloak")
                         .defaultSuccessUrl(securityProperties.getDefaultLoginSuccessUrl(), true))
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf ->
+                        csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
                 .sessionManagement(sm -> sm
                         .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 )
