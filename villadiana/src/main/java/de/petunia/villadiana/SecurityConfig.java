aa1d1fe4509a4788308874aa1f18b2e2bf34939d
Felix Roske <felix@zalando.de>
2025-06-24 22:23:14 +0200

    Add different environments

--- villadiana/src/main/java/de/petunia/villadiana/SecurityConfig.java
+++ villadiana/src/main/java/de/petunia/villadiana/SecurityConfig.java
@@ -18,7 +18,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@Profile("!dev")
+@Profile({"prod", "int"})
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
