a60eae4d070e05135bc5c2a4deb364c88eb80ec6
Felix Roske <felix@zalando.de>
2025-08-07 12:34:22 +0200

    Split ingress and secrets helm chart

--- infra/nakadi/database/nakadi/execute_db_scripts_in_container.sh
+++ infra/nakadi/database/nakadi/execute_db_scripts_in_container.sh
@@ -1,2 +1,2 @@
 find . -type f -name "*.sql" | sort |
-while read -r filename; do PGPASSWORD=nakadi psql -h localhost -U "nakadi" -d "nakadi" -a -f "$filename"; done
+while read -r filename; do PGPASSWORD=nakadi psql -h localhost -p 6432 -U "nakadi" -d "nakadi" -a -f "$filename"; done
