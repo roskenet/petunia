11ae38f71c300c15510893f41433a83228a22497
Felix Roske <felix@zalando.de>
2026-02-28 19:17:20 +0100

    Remove backups that confuse AI

--- villadiana-backup/src/main/java/de/petunia/villadiana/axillaris/AxillarisGateway.java
+++
@@ -1,9 +0,0 @@
-package de.petunia.villadiana.axillaris;
-
-import de.petunia.villadiana.PetuniaSpecies;
-
-import java.util.List;
-
-//public interface AxillarisGateway {
-//    List<PetuniaSpecies> getAllPetunias();
-//}
