e01a6fdddba085669f76ff5aa2ff96b8f6f4e02b
Felix Roske <felix@zalando.de>
2026-02-28 19:08:55 +0100

    Adjust villadiana

---
+++ villadiana/src/main/java/de/petunia/villadiana/PetuniaController.java
@@ -0,0 +1,34 @@
+package de.petunia.villadiana;
+
+import de.petunia.villadiana.axillaris.AxillarisGateway;
+import de.petunia.villadiana.websocket.NotificationDemo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+public class PetuniaController {
+    @GetMapping("/petunias")
+    public String getPetuniaSpecies() {
+        var authentication= SecurityContextHolder.getContext().getAuthentication();
+        return "{\"message": \"Hallo Welt!\"}";
+    }
+
+    @PostMapping("/petunias")
+    public PetuniaSpecies postPetuniaSpecies(@RequestBody PetuniaSpecies species) {
+        log.info("Received species: {}", species);
+        return species;
+    }
+}
