690aff6597b6ad1afbef3e85bee142609795fc66
Felix Roske <felix@zalando.de>
2026-03-01 23:28:24 +0100

    Add polonium landing page

---
+++ polonium/src/main/java/de/felixroske/polonium/PoloniumApplication.java
@@ -0,0 +1,13 @@
+package de.felixroske.polonium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoloniumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoloniumApplication.class, args);
+    }
+
+}
---
+++ polonium/src/test/java/de/felixroske/polonium/PoloniumApplicationTests.java
@@ -0,0 +1,13 @@
+package de.felixroske.polonium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoloniumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
