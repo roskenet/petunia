58b80e3ab7dc602e0b19b266316e483f4df2fdc2
Felix Roske <felix@zalando.de>
2026-03-13 22:15:46 +0100

    Use records instead of lombok

--- villadiana/src/main/java/de/roskenet/petunia/villadiana/dto/UserInfo.java
+++ villadiana/src/main/java/de/roskenet/petunia/villadiana/dto/UserInfo.java
@@ -1,24 +1,12 @@
 package de.roskenet.petunia.villadiana.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
 import java.util.List;
 import java.util.UUID;
 
-@Data
-public class UserInfo {
-    private String name;
-    private String email;
-    private UUID sub;
-    private List<String> roles;
-
-    public UserInfo(String name, String email, UUID sub, List<String> roles) {
-        this.name = name;
-        this.email = email;
-        this.sub = sub;
-        this.roles = roles;
-    }
-}
-
+public record UserInfo(
+    String name,
+    String email,
+    UUID sub,
+    List<String> roles
+) { }
 
