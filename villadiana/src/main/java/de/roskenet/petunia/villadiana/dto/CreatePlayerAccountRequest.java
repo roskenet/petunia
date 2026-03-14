c949bb8eabd9001a713ffe5503f0a47c12b6ffcc
Felix Roske <felix@zalando.de>
2026-03-14 13:26:39 +0100

    Use @Json... annotation for SNAKE_CASE

--- villadiana/src/main/java/de/roskenet/petunia/villadiana/dto/CreatePlayerAccountRequest.java
+++ villadiana/src/main/java/de/roskenet/petunia/villadiana/dto/CreatePlayerAccountRequest.java
@@ -1,6 +1,12 @@
 package de.roskenet.petunia.villadiana.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.UUID;
 
-public record CreatePlayerAccountRequest(UUID playerSubject, String playerName) {
+public record CreatePlayerAccountRequest(
+        @JsonProperty("player_subject")
+        UUID playerSubject,
+        @JsonProperty("player_name")
+        String playerName) {
 }
