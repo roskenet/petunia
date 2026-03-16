495f3919dcaff808c9c038ffb2fbb17e5262c6df
Felix Roske <felix@zalando.de>
2026-03-16 20:04:44 +0100

    Replace polonium frontend app

--- polonium/src/app/page.tsx
+++ polonium/src/app/page.tsx
@@ -6,14 +6,6 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        {/*<Image*/}
-        {/*  className="dark:invert"*/}
-        {/*  src="/next.svg"*/}
-        {/*  alt="Next.js logo"*/}
-        {/*  width={100}*/}
-        {/*  height={20}*/}
-        {/*  priority*/}
-        {/*/>*/}
         <Stack gap="lg" align="flex-start" mt="xl">
           <Title order={1} className="max-w-xs tracking-tight text-black dark:text-zinc-50">
             Willkommen!
